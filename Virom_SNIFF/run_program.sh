66642809f62540afcdde50c5e5a1a69206b1b1aa
Ubuntu <ubuntu@ip-172-16-240-152.ec2.internal>
2017-06-20 19:03:10 +0000

    commented version down to spades

--- Virom_SNIFF/run_program.sh
+++ Virom_SNIFF/run_program.sh
@@ -4,88 +4,58 @@ acc_file=$1
 data='/workdir'
 cd $data
 mkdir $data/results
-#> $data/start_log.txt
+> $data/start_log.txt
 
 data='/workdir'
 cd $data
+# read sample list file, check if started, if not begin pipeline
 cat $data/$acc_file | while read LINE
 do
     if grep $LINE "$acc_file";then
 	:
     else
 	echo $LINE >> $data/start_log.txt
-	/home/ubuntu/virus_team/sratoolkit.2.8.2-1-ubuntu64/bin/prefetch $LINE
-#	prefetch -a "/opt/aspera/connect/bin/ascp|/opt/aspera/connect/etc/asperaweb_id_dsa.openssh" $LINE
+# fetch sra file
+	/home/ubuntu/virus_team/sratoolkit.2.8.2-1-ubuntu64/bin/prefetch -a "/home/ubuntu/.aspera/connect/bin/ascp|/home/ubuntu/.aspera/connect/etc/asperaweb_id_dsa.openssh"
 	wait
 	echo $LINE.sra downloaded
+# convert to fastq format
 	fastq-dump -split-spot -W -M 20 -I $data/sra/$LINE.sra
 	echo $LINE dumped
+# check for adapters 
 	trim_galore -q -lenght $LINE.fastq
-	# map
+#search for viral like sequences usign MMseq2
 	mmseqs createdb $LINE.fastq $LINE.DNAdb
         mmseqs translatenucs $LINE.DNAdb $LINE.PROdb
         mmseqs search  $LINE.PROdb MMSEQ_DB $LINE.RESULTdb MMseq_tmp
         mmseqs convertalis $LINE.PROdb MMSEQ_DB $LINE.RESULTdb $LINE.blastp
+#filter for reads with hits
         cut -f1  $LINE.blastp | uniq > $LINE.id
         grep -A 3 -Ff $LINE.id $LINE.fastq | sed '/--/d' > $LINE_'virus'.fastq
+#take MMSeq2 output and do de novo assembly using spades
+        python /home/ubuntu/virus_team/spades/SPAdes-3.10.1-Linux/bin/spades.py  -k 11,21,31 -s $LINE_'virus’.fastq  -o $
+        cd  $LINE_’contig’
+        mv  contigs.fasta $LINE_’contig.fasta’
+        mv $LINE_’contig.fasta’ ..
+        cd ..
+        rm -rf  $LINE_’contig’
+#do blastn search on assemble contigs
+        blastn -db $database -query $contig.fa -out $outfile -evalue $par1 -perc_identity $par2 -outfmt 6 -max_target_se$
+
+#parse results and make new fasta with contigs with no hits and below threshold blast hits
+
+#do tblastx search on leftover contigs
+        tblastx  -db $database -query $blastn_no_hits.fa -out $outfile -evalue $par5 -outfmt 6 -max_target_seqs $par6 -n$
+
+#parse results and make new fasta with contigs with no hits and below threshold tblastx hits
+
+        #do CD search using Hammer search
+
+
+
 	rm $data/sra/$LINE.sra
 	rm $LINE.fastq
     fi
 done
-/home/ubuntu/ncbi/public/sra/SRR3190527.sr
 
 
-
-data='/workdir'
-cd $data
-
-#start log of analyze samples
-> $data/start_log.txt
-
-data='/workdir'
-cd $data
-
-#read sample list file
-cat $data/RNA_seq_samples.txt | while read LINE
-do
-    if grep $LINE "$data/start_log.txt";then
-	:
-    else
-	echo $LINE >> $data/start_log.txt
-	#fetch sra file
-	prefetch -a "/opt/aspera/connect/bin/ascp|/opt/aspera/connect/etc/asperaweb_id_dsa.openssh" $LINE
-	wait
-	echo $LINE.sra downloaded
-	#convert to fastq
-	fastq-dump -split-spot -W -M 20 -I $data/sra/$LINE.sra
-	echo $LINE dumped
-	#do adaptive adaptor trimming
-	trim_galore -q -lenght $LINE.fastq
-	# do enrichment for viral like sequences
-	mmseqs createdb $LINE.fastq $LINE.DNAdb
-	mmseqs translatenucs $LINE.DNAdb $LINE.PROdb
-	mmseqs search  $LINE.PROdb MMSEQ_DB $LINE.RESULTdb MMseq_tmp
-	mmseqs convertalis $LINE.PROdb MMSEQ_DB $LINE.RESULTdb $LINE.blastp
-	cut -f1  $LINE.blastp | uniq > $LINE.id
-	grep -A 3 -Ff $LINE.id $LINE.fastq | sed '/--/d' > $LINE_'virus'.fastq
-
-	#take MMSeq2 output and do de novo assembly using spades
-
-
-	#do blastn search on assemble contigs
-	blastn -db $database -query $contig.fa -out $outfile -evalue $par1 -perc_identity $par2 -outfmt 6 -max_target_seqs $par3 -num_threads $par4
-
-	#parse results and make new fasta with contigs with no hits and below threshold blast hits
-
-	#do tblastx search on leftover contigs
-	tblastx  -db $database -query $blastn_no_hits.fa -out $outfile -evalue $par5 -outfmt 6 -max_target_seqs $par6 -num_threads $par4
-
-	#parse results and make new fasta with contigs with no hits and below threshold tblastx hits
-
-	#do CD search using Hammer search
-
-	rm $data/sra/$LINE.sra
-	rm $LINE.fastq
-
-    fi
-done
